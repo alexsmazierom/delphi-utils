ae8359f6e64361484be3995924ab880168e426b2
c2d.dev <alexsmazierom@gmail.com>
2021-10-19 20:26:31 -0300

    add DelphiUtils.Hub e DelphiUtils.Genericos no Pacote_Delphi_Utils

---
+++ fontes/DelphiUtils.Genericos.pas
@@ -0,0 +1,75 @@
+﻿unit DelphiUtils.Genericos;
+
+interface
+
+uses
+{ RTL }
+  System.SysUtils, System.StrUtils, System.Generics.Collections;
+
+type
+  TDelphiUtilGenericos = class
+    class procedure Usar<T: class>(AInstanciaObj: T; AProcedimento: TProc<T>); overload; static;
+    class procedure Usar<T: class>(AInstanciaObj: T; AProcedimento: TProc<T>; ATratamentoExcecao: TProc<Exception>); overload; static;
+    class function Retornar<T,TResult>(AInstanciaObj: T; AFuncao: TFunc<T,TResult>): TResult; overload; static;
+    class function Retornar<T,TResult>(AInstanciaObj: T; AFuncao: TFunc<T,TResult>; ATratamentoExcecao: TProc<Exception>): TResult; overload; static;
+  end;
+
+implementation
+
+{ TDelphiUtilGenericos }
+
+class procedure TDelphiUtilGenericos.Usar<T>(AInstanciaObj: T; AProcedimento: TProc<T>);
+begin
+  try
+    AProcedimento(AInstanciaObj);
+  finally
+    FreeAndNil(AInstanciaObj);
+  end;
+end;
+
+class procedure TDelphiUtilGenericos.Usar<T>(AInstanciaObj: T; AProcedimento: TProc<T>;
+  ATratamentoExcecao: TProc<Exception>);
+begin
+  try
+    try
+      AProcedimento(AInstanciaObj);
+    except
+      on E: Exception do
+        if Assigned(ATratamentoExcecao) then
+          ATratamentoExcecao(E);
+        //else
+          //raise;
+    end;
+  finally
+    FreeAndNil(AInstanciaObj);
+  end;
+end;
+
+class function TDelphiUtilGenericos.Retornar<T,TResult>(AInstanciaObj: T; AFuncao: TFunc<T,TResult>): TResult;
+begin
+  try
+    Result := AFuncao(AInstanciaObj);
+  finally
+    FreeAndNil(AInstanciaObj);
+  end;
+end;
+
+class function TDelphiUtilGenericos.Retornar<T, TResult>(AInstanciaObj: T; AFuncao: TFunc<T, TResult>;
+  ATratamentoExcecao: TProc<Exception>): TResult;
+begin
+  try
+    try
+      Result := AFuncao(AInstanciaObj);
+    except
+      on E: Exception do
+        if Assigned(ATratamentoExcecao) then
+          ATratamentoExcecao(E);
+        //else
+          //raise;
+    end;
+  finally
+    FreeAndNil(AInstanciaObj);
+  end;
+end;
+
+end.
---
+++ fontes/DelphiUtils.Hub.pas
@@ -0,0 +1,16 @@
+unit DelphiUtils.Hub;
+
+interface
+
+uses
+{ RTL }
+  System.SysUtils, System.StrUtils,
+{ delphi-utils/fontes }
+  DelphiUtils.Genericos;
+
+type
+  TGenericosUtil = DelphiUtils.Genericos.TDelphiUtilGenericos;
+
+implementation
+
+end.
