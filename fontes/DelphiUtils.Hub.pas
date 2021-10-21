0646fb8c03a3b975008a70eb6954b9edadaab01b
c2d.dev <alexsmazierom@gmail.com>
2021-10-21 19:20:44 -0300

    adicionado DelphiUtils.MetodosAnonimos ao pacote

--- fontes/DelphiUtils.Hub.pas
+++ fontes/DelphiUtils.Hub.pas
@@ -6,10 +6,12 @@ uses
 { RTL }
   System.SysUtils, System.StrUtils,
 { delphi-utils/fontes }
-  DelphiUtils.Genericos;
+  DelphiUtils.Genericos,
+  DelphiUtils.MetodosAnonimos;
 
 type
   TGenericosUtil = DelphiUtils.Genericos.TDelphiUtilGenericos;
+  TMetodosAnonimosUtil = DelphiUtils.MetodosAnonimos.TDelphiUtilMetodosAnonimos;
 
 implementation
 
